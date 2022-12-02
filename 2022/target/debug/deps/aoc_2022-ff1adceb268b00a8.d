ba57a8cfb3b1eecf6f7f6acd556eaa075a8c5fb2
Tommy Treb <tomtrezb2003@gmail.com>
2022-12-01 23:35:09 -0600

    repo refactored

--- 2022/target/debug/deps/aoc_2022-ff1adceb268b00a8.d
+++
@@ -1,7 +0,0 @@
-/home/tommy/Documents/projects/advent-of-code/2022/target/debug/deps/aoc_2022-ff1adceb268b00a8: src/main.rs src/day1/mod.rs src/day1/input.txt
-
-/home/tommy/Documents/projects/advent-of-code/2022/target/debug/deps/aoc_2022-ff1adceb268b00a8.d: src/main.rs src/day1/mod.rs src/day1/input.txt
-
-src/main.rs:
-src/day1/mod.rs:
-src/day1/input.txt:
